27cbeda18e626d44e6de433d637052ca073b4c9e
piumal302 <piumal302@gmail.com>
2024-07-06 21:07:34 +0530

    connected to mongodb atlas

--- server.js
+++ server.js
@@ -1,30 +1,35 @@
+const mongoose = require('mongoose');
 const express = require('express')
-const mongoose= require ('mongoose')
-const bodyParser = require('body-parser')
-const dotenv =require('dotenv')
 
-// Load environment variables from .env file
-dotenv.config();
+const app =express()
+const port = 3000
 
-const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://fitTrackDB:hatVQCmbzGsF40RW@cluster0.tsqan0p.mongodb.net/?appName=Cluster0";
 
-// Middleware
-app.use(bodyParser.json());
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-// DB Config
-const db = process.env.MONGO_URI; 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
-// Connect to MongoDB
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
-
-// Define routes
-//app.use('/api/users', require('./routes/users'));
-//app.use('/api/schedules', require('./routes/schedules'));
-
-
-
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(port,()=>{
+    console.log(`Listerning on port: ${port}`);
+});
