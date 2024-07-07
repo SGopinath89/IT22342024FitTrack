513e6e85cfbadc0fdd569b734adb698a605086ef
piumal302 <piumal302@gmail.com>
2024-07-07 21:30:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,32 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-//const config = require('config')
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+// const config = require('config');
 
-const app =express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(bodyParser.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://fitTrackDB:hatVQCmbzGsF40RW@cluster0.tsqan0p.mongodb.net/?appName=Cluster0";
+const MONGO_URI = "mongodb+srv://fitTrackDB:hatVQCmbzGsF40RW@cluster0.tsqan0p.mongodb.net/?appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+mongoose.connect(MONGO_URI, {
+  connectTimeoutMS: 10000,
+  serverSelectionTimeoutMS: 10000,
+  socketTimeoutMS: 45000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log("Successfully connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/schedules', require('./routes/scheduleRoutes'));
 
-app.listen(port,()=>{
-    console.log(`Listerning on port: ${port}`);
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
 });
